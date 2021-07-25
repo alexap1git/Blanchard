0afcc874d289f4985160dd955eea75b28c95b9e0
Александр Попов <alexap1.1976@gmail.com>
2021-07-25 23:18:22 +0300

    Header+ touch to beautiful (adaptive)

--- js/index.js
+++ js/index.js
@@ -1,25 +1,23 @@
 window.addEventListener('DOMContentLoaded', function () {
 
                 document.querySelector('.search__realizm').addEventListener('click', function(){
-                    document.querySelector('.is-activ').classList.remove('is-activ')
-                    document.querySelector('.menu__realizm').classList.toggle('is-activ')
+                    document.querySelector('.menu__realizm').classList.toggle('realizm__is-activ')
                 })
                 document.querySelector('.search__impressianizm').addEventListener('click', function(){
-                    document.querySelector('.is-activ').classList.remove('is-activ')
-                    document.querySelector('.menu__impressianizm').classList.toggle('is-activ')
+                    document.querySelector('.menu__impressianizm').classList.toggle('impressianizm__is-activ')
                 })
                 document.querySelector('.search__postimpressianizm').addEventListener('click', function(){
-                    document.querySelector('.is-activ').classList.remove('is-activ')
-                    document.querySelector('.menu__postimpressianizm').classList.toggle('is-activ')
+                    document.querySelector('.menu__postimpressianizm').classList.toggle('postimpressianizm__is-activ')
                 })
                 document.querySelector('.search__avangard').addEventListener('click', function(){
-                    document.querySelector('.is-activ').classList.remove('is-activ')
-                    document.querySelector('.menu__avangard').classList.toggle('is-activ')
+                    document.querySelector('.menu__avangard').classList.toggle('avangard__is-activ')
                 })
                 document.querySelector('.search__futurism').addEventListener('click', function(){
-                    document.querySelector('.is-activ').classList.remove('is-activ')
-                    document.querySelector('.menu__futurism').classList.toggle('is-activ')
+                    document.querySelector('.menu__futurism').classList.toggle('futurism__is-activ')
                 })
-   
+                document.querySelector('#burger').addEventListener('click', function(){
+                    document.querySelector('#nav-menu').classList.toggle('nav__is-activ')
+                    document.querySelector('.header__burger-btn').classList.toggle('burger__is-activ')
+                })
 
 })
